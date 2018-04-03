2e95544d0c9d4ee1bed31b9e39360c5f563516db
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2018-04-03 11:34:12 +0900

    serialize

---
+++ serialize/src/com/example/DeserializeObject.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+public class DeserializeObject {
+
+    public static void main(String[] args) throws Exception {
+
+        FileInputStream fileIn = new FileInputStream("map.ser");
+        ObjectInputStream in = new ObjectInputStream(fileIn);
+        Object object = in.readObject();
+        
+        System.out.println(object);
+        
+        in.close();
+        fileIn.close();
+    }
+}
---
+++ serialize/src/com/example/SerializeObject.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+
+public class SerializeObject {
+
+    public static void main(String[] args) throws Exception {
+
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("key1", new Person("John", 35));
+        map.put("key2", "ABC");
+
+        FileOutputStream fileOut = new FileOutputStream("map.ser");
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+        out.writeObject(map);
+        out.close();
+        fileOut.close();
+    }
+}
